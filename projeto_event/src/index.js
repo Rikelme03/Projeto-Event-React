ce728fe016eafb187b515ccf41e4174b87235902
RIKELME MIRANDA SILVA <47540013893@info.local>
2025-05-06 11:37:53 -0300

    Feat:Subindo com a tela de login responsividade feitas e com os componentes

---
+++ projeto_event/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Login from './pages/login/Login';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Lista from './components/lista/Lista';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Lista/>
+  </React.StrictMode>
+);
+
---
+++ projeto_event/src/pages/login/Login.jsx
@@ -0,0 +1,41 @@
+import "./Login.css";
+import LogoEvent from "../../assets/logoEvent.svg"
+import Logo from "../../assets/logo.png"
+import Botao from "../../components/botao/Botao";
+
+const Login = () =>{
+    return (
+        <main className="main_login">
+            <div className="logoBanner"> 
+                <img src={Logo} alt="" />
+
+            </div>
+            <section className="section_login">
+
+                <form action="" className="form_login">
+                <img src= {LogoEvent} alt="Logo do event+" />
+                
+                <div className="campos_login">
+
+                <div className="campo_input">
+                    <input type="nome" name="nome" placeholder="Username" />
+                </div>
+
+                <div className="campo_input">
+                    <input type="nome" name="nome" placeholder="Password" />
+                </div>
+                </div>
+                <a href="">Esqueceu a senha</a>
+                <Botao nomeDoBotao = "Cadastrar"/>
+                </form>
+                
+
+
+
+
+            </section>
+        </main>
+    )
+}
+
+export default Login;
