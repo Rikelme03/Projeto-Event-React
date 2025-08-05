964e950c56b550f3af287c61f86c42725c3ccdb0
RIKELME MIRANDA SILVA <47540013893@info.local>
2025-08-05 16:16:16 -0300

    Feat: Subinco com azure servicos, pronto para o deploy

--- projeto_event/src/Services/services.js
+++ projeto_event/src/Services/services.js
@@ -4,9 +4,11 @@ const apiPorta = "5289"
 
 const apiLocal = `http://localhost:${apiPorta}/api/`;
 
+const apiAzure= "https://apieventrikelme-bfemd7gvh6eshegb.brazilsouth-01.azurewebsites.net/api/";
+
 const api = axios.create({
 
-    baseURL: apiLocal
+    baseURL: apiAzure
 
 }); 
 
